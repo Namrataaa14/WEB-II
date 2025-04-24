cffdd306373274bfa30aff58ec878aaef0c373c5
Namrata <namratabastola500@gmail.com>
2025-04-24 19:57:18 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,190 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=ec, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<style>
+#hea{
+    background-color:pink;
+padding:20px;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</style>
+
+
+
+
+
+
+
+<body>
+    <header id="hea">
+<h2>Info</h2>
+
+    </header>
+    <form>
+        <label>Name:</label>
+        <input type="text" placeholder="">
+        <br><br>
+
+
+        <label>Address:</label>
+        <input type="text" placeholder="">
+        <br><br>
+
+
+        <label>Age:</label>
+        <input type="number" placeholder="">
+        <br><br>
+
+        <label for="Gender">Gender:</label>
+        <label name ="Gender">Male</label>
+        <input type="radio" name="Gender">
+        <label name ="Gender">Female</label>
+
+        <input type="radio" name="Gender">
+        <br><br>
+
+        <input type="submit">
+
+
+
+
+
+
+
+
+
+
+
+    </form>
+
+    <footer id="foo">
+        <P>This is a php </P>
+
+
+
+
+
+
+    </footer>
+
+</body>
+</html> -->
+
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=<<, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+
+  <style>
+    body {
+      background-color: #bddde4;
+    }
+    .header-class {
+      background-color: #9fb3df;
+      padding: 9px;
+      text-align: center;
+      color: rgb(0, 0, 0);
+    }
+    .footer-class {
+      background-color: #9fb3df;
+      padding: 9px;
+      text-align: center;
+      bottom: 0;
+      position: fixed;
+      width: 100%;
+    }
+    form {
+      background-color: rgb(244, 244, 244);
+      padding: 20px;
+      max-width: 400px;
+      margin: 25px auto;
+      border-radius: 10px;
+      align-items: center;
+      height: 200px;
+    }
+  </style>
+  <body>
+    <header class="header-class"><h2>Student</h2></header>
+    <form>
+      <label>Name:</label> 
+      <input type="text" placeholder="" />
+      <br /><br />
+
+      <label>Address:</label>
+      <input type="text" placeholder="" />
+      <br /><br />
+
+      <label for="Gender">Gender:</label>
+      <label name="Gender"> Male </label>
+      <input type="radio" name="Gender" checked />
+      <label name="Gender"> female </label>
+      <input type="radio" name="Gender" />
+      <br /><br />
+
+      <label>File upload:</label>
+      <input type="file" placeholder="" />
+      <br /><br />
+
+      <input type="Submit" />
+    </form>
+
+
+
+    <?php
+    echo"Hello";
+
+    $age = 20;
+    if($age>=20){
+        echo"You can drink beer.";
+    }
+    else{  
+        echo "You cant";
+
+    }
+
+
+
+$age = array(14,23,67);
+echo "The age is: ";
+echo"$age[0]";
+    for($i = 0; $i< 20; $i++){
+      echo "$age";
+
+     ?>
+
+
+
+
+
+    <footer class="footer-class">@2025 Namrata Bastola</footer>
+  </body>
+</html>
